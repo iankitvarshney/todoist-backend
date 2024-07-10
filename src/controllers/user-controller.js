9aacc6eac54e12b8576c1ca4c8682730e84f3385
Ankit Varshney <ankit.varshney.28.2@mountblue.tech>
2024-07-10 12:03:49 +0530

    Added API for create user

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -12,14 +12,33 @@ async function getAll(req, res) {
 
     SuccessResponse.message = "Successfully fetched all users";
     SuccessResponse.data = users;
+
     return res.status(200).json(SuccessResponse);
   } catch (error) {
     ErrorResponse.message = "Something went wrong while fetching all users";
     ErrorResponse.error = error;
+
+    return res.status(500).json(ErrorResponse);
+  }
+}
+
+async function create(req, res) {
+  try {
+    const user = await User.create(req.body);
+
+    SuccessResponse.message = "Successfully created a user";
+    SuccessResponse.data = user;
+
+    return res.status(201).json(SuccessResponse);
+  } catch (error) {
+    ErrorResponse.message = "Something went wrong while creating a user";
+    ErrorResponse.error = error;
+
     return res.status(500).json(ErrorResponse);
   }
 }
 
 module.exports = {
   getAll,
+  create,
 };
--- src/routes/v1/userRoutes.js
+++ src/routes/v1/userRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 
-const { getAll } = require("../../controllers/user-controller");
+const { validateCreateRequest } = require("../../middlewares/user-middlewares");
+const { getAll, create } = require("../../controllers/user-controller");
 
 const router = express.Router();
 
 router.get("/", getAll);
+router.post("/", validateCreateRequest, create);
 
 module.exports = router;
