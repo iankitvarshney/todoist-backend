1fdfc153d56f85b0c8d42267165e23cef9be45e0
Ankit Varshney <ankit.varshney.28.2@mountblue.tech>
2024-07-10 17:33:20 +0530

    Added API for destroy section

--- src/controllers/section-controller.js
+++ src/controllers/section-controller.js
@@ -131,9 +131,36 @@ async function update(req, res) {
   }
 }
 
+async function destroy(req, res) {
+  try {
+    const response = await Section.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (response === 0) {
+      SuccessResponse.message = `No section is available with id ${req.params.id}`;
+      SuccessResponse.data = {};
+      return res.status(404).json(SuccessResponse);
+    }
+
+    SuccessResponse.message = "Successfully destroyed a section";
+    SuccessResponse.data = {};
+
+    return res.status(200).json(SuccessResponse);
+  } catch (error) {
+    ErrorResponse.message = "Something went wrong while destroying a section";
+    ErrorResponse.error = error;
+
+    return res.status(500).json(ErrorResponse);
+  }
+}
+
 module.exports = {
   getAll,
   get,
   create,
   update,
+  destroy,
 };
