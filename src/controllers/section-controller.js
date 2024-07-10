38f528191cbd3c0f64f92605c8650bd5a61a7a70
Ankit Varshney <ankit.varshney.28.2@mountblue.tech>
2024-07-10 17:15:06 +0530

    Added API for get section

--- src/controllers/section-controller.js
+++ src/controllers/section-controller.js
@@ -31,6 +31,28 @@ async function getAll(req, res) {
   }
 }
 
+async function get(req, res) {
+  try {
+    const section = await Section.findByPk(req.params.id);
+
+    if (section === null) {
+      SuccessResponse.message = `No section is available with id ${req.params.id}`;
+      SuccessResponse.data = {};
+      return res.status(404).json(SuccessResponse);
+    }
+
+    SuccessResponse.message = "Successfully fetched a section";
+    SuccessResponse.data = section;
+
+    return res.status(200).json(SuccessResponse);
+  } catch (error) {
+    ErrorResponse.message = "Something went wrong while fetching a section";
+    ErrorResponse.error = error;
+
+    return res.status(500).json(ErrorResponse);
+  }
+}
+
 async function create(req, res) {
   try {
     const section = await Section.create(req.body);
@@ -63,5 +85,6 @@ async function create(req, res) {
 
 module.exports = {
   getAll,
+  get,
   create,
 };
--- src/routes/v1/sectionRoutes.js
+++ src/routes/v1/sectionRoutes.js
@@ -3,11 +3,12 @@ const express = require("express");
 const {
   validateCreateRequest,
 } = require("../../middlewares/section-middlewares");
-const { getAll, create } = require("../../controllers/section-controller");
+const { getAll, get, create } = require("../../controllers/section-controller");
 
 const router = express.Router();
 
 router.get("/", getAll);
+router.get("/:id", get);
 router.post("/", validateCreateRequest, create);
 
 module.exports = router;
