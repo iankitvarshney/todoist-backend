78bab4446d057dd7f800343f9c17e24aeb5a7995
Ankit Varshney <ankit.varshney.28.2@mountblue.tech>
2024-07-14 16:47:05 +0530

    Enabled CORS Requests

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
 
 const apiRoutes = require("./routes");
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
